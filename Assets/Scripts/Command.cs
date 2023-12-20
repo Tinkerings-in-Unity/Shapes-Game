ca47a0c4daf8d2fffba1947236b5586a70582906
Gerald <gombirog@gmail.com>
2023-12-20 14:16:25 +0200

    Command are now rearrangable

--- Assets/Scripts/Command.cs
+++ Assets/Scripts/Command.cs
@@ -4,20 +4,50 @@ using UnityEngine.UI;
 public class Command : MonoBehaviour
 {
 
+    [SerializeField]
     protected Button deleteButton;
+    [SerializeField]
+    protected Button upButton;
+    [SerializeField]
+    protected Button downButton;
 
     protected GameController gameController;
 
     protected virtual void Start()
     {
         gameController = FindObjectOfType<GameController>();
-        Button deleteButton =  GetComponentInChildren<Button>();
         deleteButton.onClick.AddListener(OnDeleteButtonClick);
+        upButton.onClick.AddListener(OnUpButtonClick);
+        downButton.onClick.AddListener(OnDownButtonClick);
     }
 
     protected void OnDeleteButtonClick()
     {
-        Destroy(this.gameObject);
+        Destroy(gameObject);
+    }
+
+    protected void OnUpButtonClick()
+    {
+        var originalIndex = transform.GetSiblingIndex();
+        originalIndex --;
+
+        var parent = transform.parent;
+
+        if(parent.GetChild(originalIndex)){
+            transform.SetSiblingIndex(originalIndex);
+        }
+    }
+
+    protected void OnDownButtonClick()
+    {
+        var originalIndex = transform.GetSiblingIndex();
+        originalIndex ++;
+
+        var parent = transform.parent;
+
+        if(parent.GetChild(originalIndex)){
+            transform.SetSiblingIndex(originalIndex);
+        }
     }
 
     public virtual void Execute()
